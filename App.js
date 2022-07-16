18a565c0f728b9da612a3ca48c1eac8d224f24e0
Abhishek Kokate <abhishekkokate8@gmail.com>
2022-07-16 21:53:40 +0530

    Card Done

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import styled from "styled-components";
 import myLogo from "./assets/icon.png";
 import Card from "./components/Card";
 
 export default function App() {
-  const [count, setCount] = useState(0);
   return (
     <>
       <Container>
@@ -16,65 +15,22 @@ export default function App() {
             <Title>Welcome to React Native!</Title>
             <Name>Abhishek</Name>
           </TitleContaier>
-          <TouchableOpacity
-            style={{
-              backgroundColor: "#2196f3",
-              justifyContent: "center",
-              alignItems: "center",
-              width: 50,
-              height: 50,
-              borderRadius: 50,
-              elevation: 4,
-              marginLeft: "auto",
-            }}
-            onPress={() => {
-              setCount(count + 1);
-            }}
-          >
-            <Text
-              style={{
-                color: "white",
-                fontWeight: "bold",
-              }}
-            >
-              +
-            </Text>
-          </TouchableOpacity>
         </TitleBar>
-        <CountContainer>
-          <Title style={{ fontSize: 20 }}>Count is:</Title>
-          <Name style={{ fontSize: 40 }}>{count}</Name>
-        </CountContainer>
-        {/* <Card /> */}
+        <Subtitle>Continue Learining</Subtitle>
+        <Card />
         <StatusBar style="auto" />
       </Container>
     </>
   );
 }
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #f0f3f5;
-`;
-
-const Title = styled.Text`
-  font-size: 16px;
-  color: #0008;
-`;
-
-const Name = styled.Text`
-  font-size: 20px;
-  color: #000;
+const Subtitle = styled.Text`
+  color: #b8bece;
   font-weight: 600;
-`;
-
-const TitleBar = styled.View`
-  width: 90%;
-  margin: 10px;
-  margin-top: 40px;
-  flex-direction: row;
-  align-items: center;
-  /* background: #ff0; */
+  font-size: 15px;
+  margin-top: 50px;
+  margin-left: 20px;
+  text-transform: uppercase;
 `;
 
 const TitleContaier = styled.View``;
@@ -86,16 +42,25 @@ const Avatar = styled.Image`
   border-radius: 50px;
   margin: 1% 3%;
 `;
-
-const TitleButton = styled.Button`
-  width: 50px;
-  border-radius: 50%;
+const TitleBar = styled.View`
+  width: 90%;
   margin: 10px;
+  margin-top: 40px;
+  flex-direction: row;
+  align-items: center;
+  /* background: #ff0; */
+`;
+const Name = styled.Text`
+  font-size: 20px;
+  color: #000;
+  font-weight: 600;
+`;
+const Title = styled.Text`
+  font-size: 16px;
+  color: #0008;
 `;
 
-const CountContainer = styled.View`
-  font-size: 30px;
+const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
+  background-color: #f0f3f5;
 `;
--- components/Card.js
+++ components/Card.js
@@ -4,8 +4,76 @@ import styled from "styled-components";
 
 export default function Card() {
   return (
-    <View>
-      <Text>Card</Text>
-    </View>
+    <Container>
+      <Cover>
+        <Image source={require("../assets/background2.jpg")}></Image>
+        <Title>Styled Components</Title>
+      </Cover>
+      <Content>
+        <Logo source={require("../assets/logo-react.png")}></Logo>
+        <Wrapper>
+          <Caption>React Native</Caption>
+          <Subtitle>5 of 12 sections</Subtitle>
+        </Wrapper>
+      </Content>
+    </Container>
   );
 }
+
+const Subtitle = styled.Text`
+  color: #0008;
+`;
+
+const Caption = styled.Text`
+  color: #000a;
+  font-weight: 600;
+  font-size: 20px;
+`;
+
+const Logo = styled.Image`
+  width: 50px;
+  height: 50px;
+`;
+
+const Wrapper = styled.View`
+  margin-left: 10px;
+`;
+
+const Content = styled.View`
+  flex-direction: row;
+  padding-left: 20px;
+  align-items: center;
+  height: 80px;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: 600;
+  margin: 20px;
+  width: 170px;
+`;
+
+const Image = styled.Image`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Cover = styled.View`
+  width: 100%;
+  height: 200px;
+  border-radius: 14px;
+`;
+
+const Container = styled.View`
+  background: white;
+  width: 315px;
+  height: 280px;
+  border-radius: 14px;
+  margin: 20px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+  overflow: hidden;
+`;
