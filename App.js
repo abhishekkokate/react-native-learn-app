f9d8f4b3fd0b93317dfeca16448c7a8eb068a766
Abhishek Kokate <abhishekkokate8@gmail.com>
2022-07-16 17:59:34 +0530

    Counter App done

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import styled from "styled-components";
 import myLogo from "./assets/icon.png";
 import Card from "./components/Card";
 
 export default function App() {
-  console.log("App Started");
+  const [count, setCount] = useState(0);
   return (
     <>
       <Container>
@@ -15,7 +16,35 @@ export default function App() {
             <Title>Welcome to React Native!</Title>
             <Name>Abhishek</Name>
           </TitleContaier>
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#2196f3",
+              justifyContent: "center",
+              alignItems: "center",
+              width: 50,
+              height: 50,
+              borderRadius: 50,
+              elevation: 4,
+              marginLeft: "auto",
+            }}
+            onPress={() => {
+              setCount(count + 1);
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontWeight: "bold",
+              }}
+            >
+              +
+            </Text>
+          </TouchableOpacity>
         </TitleBar>
+        <CountContainer>
+          <Title style={{ fontSize: 20 }}>Count is:</Title>
+          <Name style={{ fontSize: 40 }}>{count}</Name>
+        </CountContainer>
         {/* <Card /> */}
         <StatusBar style="auto" />
       </Container>
@@ -40,7 +69,7 @@ const Name = styled.Text`
 `;
 
 const TitleBar = styled.View`
-  width: 100%;
+  width: 90%;
   margin: 10px;
   margin-top: 40px;
   flex-direction: row;
@@ -56,4 +85,17 @@ const Avatar = styled.Image`
   background: #000;
   border-radius: 50px;
   margin: 1% 3%;
+`;
+
+const TitleButton = styled.Button`
+  width: 50px;
+  border-radius: 50%;
+  margin: 10px;
+`;
+
+const CountContainer = styled.View`
+  font-size: 30px;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
