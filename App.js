6dfb72c92a16c3ef0fcfa500f7a2402248f76b8d
Abhishek Kokate <abhishekkokate8@gmail.com>
2022-07-16 16:51:04 +0530

    TitleBar Done

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import styled from "styled-components";
+import myLogo from "./assets/icon.png";
+import Card from "./components/Card";
 
 export default function App() {
+  console.log("App Started");
   return (
-    <View style={styles.container}>
-      <Text>Welcome to React Native</Text>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Container>
+        <TitleBar>
+          <Avatar source={require("./assets/avatar.jpg")}></Avatar>
+          <TitleContaier>
+            <Title>Welcome to React Native!</Title>
+            <Name>Abhishek</Name>
+          </TitleContaier>
+        </TitleBar>
+        {/* <Card /> */}
+        <StatusBar style="auto" />
+      </Container>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f7f7f7",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #0008;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #000;
+  font-weight: 600;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin: 10px;
+  margin-top: 40px;
+  flex-direction: row;
+  align-items: center;
+  /* background: #ff0; */
+`;
+
+const TitleContaier = styled.View``;
+
+const Avatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  background: #000;
+  border-radius: 50px;
+  margin: 1% 3%;
+`;
---
+++ components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styled from "styled-components";
+
+export default function Card() {
+  return (
+    <View>
+      <Text>Card</Text>
+    </View>
+  );
+}
